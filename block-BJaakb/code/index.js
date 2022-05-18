abd4b5a69fb9c670751efbfa50cc6e8b407a77d5
raj shringi <raj24shringi@gmail.com>
2022-05-18 20:33:40 +0530

    make code more readable

--- block-BJaakb/code/index.js
+++ block-BJaakb/code/index.js
@@ -1,34 +1,36 @@
 function fetch(url) {
-    return new Promise((resolve, reject) => {
-      let xhr = new XMLHttpRequest();
-      xhr.open("GET", url);
-      xhr.onload = () => resolve(JSON.parse(xhr.response));
-      xhr.onerror = () => reject(`couldn't load the data`);
-      xhr.send();
-    });
-  }
-  
-  let input = document.querySelector("input");
-  let root = document.querySelector(".root");
-  function displayImage(data) {
-    root.innerHTML = "";
-    data.forEach((d) => {
-      let li = document.createElement("li");
-      let img = document.createElement("img");
-      img.src = d.urls.small;
-      img.alt = d.description;
-      li.append(img);
-      root.append(li);
-    });
-  }
-  function handleInput(e) {
-    if (e.keyCode === 13 && e.target.value !== "") {
-      fetch(`https://api.unsplash.com/search/photos/?page=1&per_page=15&query=${e.target.value}&client_id=pY9lcHLMUo2rFmDm4CsgviWqnBz4GSgVgXxiojZGWko`).then(data => displayImage(data.results))
-      e.target.value = "";
-    }
-  }
-  input.addEventListener("keyup", handleInput);
-  
+  return new Promise((resolve, reject) => {
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", url);
+    xhr.onload = () => resolve(JSON.parse(xhr.response));
+    xhr.onerror = () => reject(`couldn't load the data`);
+    xhr.send();
+  });
+}
 
-  fetch("https://api.unsplash.com/photos/?client_id=pY9lcHLMUo2rFmDm4CsgviWqnBz4GSgVgXxiojZGWko").then(data => displayImage(data));
-  
+let input = document.querySelector("input");
+let root = document.querySelector(".root");
+function displayImage(data) {
+  root.innerHTML = "";
+  data.forEach((d) => {
+    let li = document.createElement("li");
+    let img = document.createElement("img");
+    img.src = d.urls.small;
+    img.alt = d.description;
+    li.append(img);
+    root.append(li);
+  });
+}
+function handleInput(e) {
+  if (e.keyCode === 13 && e.target.value !== "") {
+    fetch(
+      `https://api.unsplash.com/search/photos/?page=1&per_page=15&query=${e.target.value}&client_id=pY9lcHLMUo2rFmDm4CsgviWqnBz4GSgVgXxiojZGWko`
+    ).then((data) => displayImage(data.results));
+    e.target.value = "";
+  }
+}
+input.addEventListener("keyup", handleInput);
+
+fetch(
+  "https://api.unsplash.com/photos/?client_id=pY9lcHLMUo2rFmDm4CsgviWqnBz4GSgVgXxiojZGWko"
+).then((data) => displayImage(data));
