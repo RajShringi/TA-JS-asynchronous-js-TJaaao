525a992d317e590eb76a3a822d3907fe07fb9dc0
raj shringi <raj24shringi@gmail.com>
2022-05-24 00:16:10 +0530

    add functionality to edit todo and fix checkbox problem

--- block-BJaakh/code/index.js
+++ block-BJaakh/code/index.js
@@ -1,26 +1,49 @@
 let url = "https://basic-todo-api.vercel.app/api/todo";
 let todosUl = document.querySelector(".todos");
 let input = document.querySelector("input");
-
+let put = false;
 function handleDelete(e) {
   let id = e.target.dataset.id;
   fetch(url + `/${id}`, {
     method: "DELETE",
     headers: { "Content-Type": "application/json" },
-  });
+  }).then(() => init());
 }
 
-function handleCheckbox(e) {
+function handleCheckbox(e, status) {
   let id = e.target.dataset.id;
   let data = {
     todo: {
-      isCompleted: true,
+      isCompleted: !status,
     },
   };
   fetch(url + `/${id}`, {
     method: "PUT",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(data),
+  }).then(() => init());
+}
+
+function updateTodo(e) {
+  let inputElm = document.createElement("input");
+  inputElm.value = e.target.innerText;
+  let p = e.target;
+  let parent = e.target.parentElement;
+  let id = e.target.dataset.id;
+  parent.replaceChild(inputElm, p);
+  inputElm.addEventListener("keyup", (e) => {
+    if (e.keyCode === 13 && e.target.value) {
+      let data = {
+        todo: {
+          title: e.target.value,
+        },
+      };
+      fetch(url + `/${id}`, {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data),
+      }).then(() => init());
+    }
   });
 }
 
@@ -34,9 +57,13 @@ function renderTodos(todos) {
     checkbox.type = "checkbox";
     checkbox.checked = todo.isCompleted;
     checkbox.setAttribute("data-id", todo._id);
-    checkbox.addEventListener("input", handleCheckbox);
+    checkbox.addEventListener("input", (e) =>
+      handleCheckbox(e, todo.isCompleted)
+    );
     let p = document.createElement("p");
     p.innerText = todo.title;
+    p.setAttribute("data-id", todo._id);
+    p.addEventListener("dblclick", updateTodo);
     div.append(checkbox, p);
     let span = document.createElement("span");
     span.innerText = "❌";
@@ -60,7 +87,7 @@ function handleInput(e) {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(data),
-    });
+    }).then(() => init());
     e.target.value = "";
   }
 }
